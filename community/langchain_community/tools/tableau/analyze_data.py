544efd8f6eb2345b2f7942aeb491e7ab67a09cf4
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2025-01-14 23:52:26 -0700

    phased generation of queries

--- community/langchain_community/tools/tableau/analyze_data.py
+++ community/langchain_community/tools/tableau/analyze_data.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict, Annotated, Optional
+from typing import Dict, Annotated
 
 from langchain_core.tools import tool
 from langchain_core.prompts import ChatPromptTemplate
@@ -9,8 +9,8 @@ from langchain_openai import ChatOpenAI
 
 from langgraph.prebuilt import InjectedState
 
-from community.langchain_community.tools.tableau.prompts import vds_prompt
-from community.langchain_community.utilities.tableau.analyze_data import AnalyzeDataInputs, augment_datasource_metadata, get_headlessbi_data
+from community.langchain_community.tools.tableau.prompts import vds_prompt, vds_prompts
+from community.langchain_community.utilities.tableau.analyze_data import AnalyzeDataInputs, augment_datasource_metadata, get_filter_values, get_headlessbi_data
 
 @tool("analyze_tableau_datasource")
 async def analyze_data(
@@ -43,24 +43,55 @@ async def analyze_data(
         # Lets the Agent know that the LUID is missing and it needs to use an alternative tool
         raise KeyError("The Datasource LUID is missing. Use a data source search tool to find an appropriate query target that matches the user query.")
 
-    # 1. Initialize Langchain chat template with an augmented prompt containing metadata for the datasource
-    query_data_prompt = ChatPromptTemplate.from_messages([
-        SystemMessage(content = await augment_datasource_metadata(
-            api_key = tableau_auth,
-            url = tableau_url,
-            datasource_luid = tableau_datasource,
-            prompt = vds_prompt
-        )),
-        ("user", "{utterance}")
-    ])
+    # Obtain metadata about the data source fields and sample values
+    datasource_metadata = await augment_datasource_metadata(
+        api_key = tableau_auth,
+        url = tableau_url,
+        datasource_luid = tableau_datasource,
+        prompt = vds_prompt
+    )
 
-    # 2. Instantiate language model and execute the prompt to write a VizQL Data Service query
+    # add the metadata to prompts used for each generation phase
+    vds_prompts['fields_prompt'] += datasource_metadata
+    vds_prompts['filters_prompt'] += datasource_metadata
+    vds_prompts['calculations_prompt'] += datasource_metadata
+
+    # Instantiate language model to execute query generation prompts
     query_writer = ChatOpenAI(
         model=os.getenv('TOOLING_MODEL'),
         temperature=0
     )
 
-    # 3. Query data from Tableau's VizQL Data Service using the dynamically written payload
+    # Langchain chat template for writing fields
+    fields_prompt = ChatPromptTemplate.from_messages([
+        SystemMessage(vds_prompts['fields_prompt']),
+        ("user", "{utterance}")
+    ])
+
+    # Augments the filtering prompt dynamically with categorical filters
+    async def filter_values(fields_generation):
+        values = await get_filter_values(
+            api_key = tableau_auth,
+            url = tableau_url,
+            datasource_luid = tableau_datasource,
+            payload = fields_generation.content
+        )
+        vds_prompts['filters_prompt'] += values
+
+        # Langchain chat template for writing filters
+        filters_prompt = ChatPromptTemplate.from_messages([
+            SystemMessage(vds_prompts['filters_prompt']),
+            ("user", "{utterance}")
+        ])
+        return filters_prompt
+
+    # Langchain chat template for writing calculations
+    calculations_prompt = ChatPromptTemplate.from_messages([
+        SystemMessage(vds_prompts['calculations_prompt']),
+        ("user", "{utterance}")
+    ])
+
+    # Query data from Tableau's VizQL Data Service using the dynamically written payload
     async def get_data(vds_query):
         return await get_headlessbi_data(
             api_key = tableau_auth,
@@ -70,7 +101,7 @@ async def analyze_data(
         )
 
     # this chain defines the flow of data through the system
-    chain = query_data_prompt | query_writer | get_data
+    chain = fields_prompt | query_writer | filter_values | query_writer | calculations_prompt | query_writer | get_data
 
     # invoke the chain to generate a query and obtain data
     vizql_data = await chain.ainvoke(query)
